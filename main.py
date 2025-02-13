6897f7a1c01fb6d613c18a4fc3a852efe6a3192f
eddie g. <eduardgavrila47@gmail.com>
2025-02-13 11:09:42 +0100

    test

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
 
     lastid = 0
     read_recently = False
+    failed = False
 
     while True:
         if not read_recently:
@@ -15,7 +16,7 @@ def main():
             read_recently = True
 
         tag_id = reader._read_id()
-        if lastid!=tag_id:
+        if lastid!=tag_id or failed:
             lastid=tag_id
 
             print(f'Read UID: {tag_id} at {time.localtime()}')
@@ -27,10 +28,13 @@ def main():
                 response = requests.get(url, headers=headers, verify=False)
                 if response.status_code == 200:
                     print(response.json())
+                    failed = False
                 else:
                     print("\nError sending Data")
+                    failed = True
             except Exception as e:
                 print("Exception:", str(e))
+                failed = True
 
             read_recently = False
 
