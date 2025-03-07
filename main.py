b50a463f68138b2937d7c3ff00e0770ae58085fd
eddie g. <eduardgavrila47@gmail.com>
2025-03-07 11:42:12 +0100

    fixed a cast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from time import sleep
+from xml.etree.ElementTree import tostring
+
 from mfrc522 import SimpleMFRC522
 import time
 import requests
@@ -30,11 +32,10 @@ def main():
             }
             try:
                 response = requests.post(url, headers=headers, json=data, verify=False)
-                print(response.status_code)
                 if response.status_code == 500:
                     print('UID doesnt exist!')
                 elif response.status_code == 200:
-                    print('Round logged for UID' + tag_id + "at " + time.asctime())
+                    print('Round logged for UID' + str(tag_id) + "at " + str(time.asctime()))
                 else:
                     raise RuntimeWarning("Unexpected response from server; code:" + response.status_code )
             except Exception as e:
